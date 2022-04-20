065e7318018714dd80f359b009df75301c5f381d
unknown <jacobdcutts@gmail.com>
2022-04-20 12:15:54 +0100

    added stylesheet template and created landing page and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Projects from "./components/Projects";
+import Header from "./components/Header";
 
 function App(): JSX.Element {
   return (
     <div className="app">
+      <Header />
       <Projects />;
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,79 @@
+import {Fade} from "react-awesome-reveal"
+import ParticlesBg from "particles-bg";
+
 export default function Header(): JSX.Element {
-  return <h1>Jacob Portfolio</h1>;
+  return (
+    <header id="Header">
+      <ParticlesBg type="cobweb" bg={true} />
+
+      <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
+          </a>
+
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#resume">
+                Resume
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#portfolio">
+                Works
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#contact">
+                Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="row banner">
+          <div className="banner-text">
+            <Fade direction="up" duration={2000}>
+              <h1 className="responsive-headline">Jacob Cutts</h1>
+            </Fade>
+            <Fade direction="up" duration={3000}>
+              <h3>Hello</h3>
+            </Fade>
+            <hr />
+            {/* <Fade bottom duration={2000}>
+              <ul className="social">
+                <a href={project} className="button btn project-btn">
+                  <i className="fa fa-book"></i>Project
+                </a>
+                <a href={github} className="button btn github-btn">
+                  <i className="fa fa-github"></i>Github
+                </a>
+              </ul>
+            </Fade> */}
+          </div>
+        </div>
+        
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+    </header>
+  );
 }
