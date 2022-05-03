9aa530dc1734d5db22ba05d0ce14f4f561c8220c
unknown <jacobdcutts@gmail.com>
2022-05-03 17:49:15 +0100

    added embeded notion page iframe

--- src/components/projects/FireworksProject.tsx
+++ src/components/projects/FireworksProject.tsx
@@ -13,7 +13,9 @@ export default function FireworksProject(): JSX.Element {
         <nav id="nav-wrap">
           <ul id="nav-project" className="nav">
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/" className="home-link">
+                Home
+              </Link>
             </li>
           </ul>
         </nav>
@@ -22,10 +24,10 @@ export default function FireworksProject(): JSX.Element {
           <div className="banner-text">
             <Fade direction="up">
               <div className="project-about-comments">
-                <a className="project-nav-button" href="#About">
+                <a className="project-nav-button" href="#about">
                   <button className="project-nav-button"> About </button>
                 </a>
-                <a className="project-nav-button" href="#Comments">
+                <a className="project-nav-button" href="#comments">
                   <button className="project-nav-button">Comments</button>
                 </a>
               </div>
@@ -41,14 +43,14 @@ export default function FireworksProject(): JSX.Element {
         </div>
       </header>
 
-      <section id="test">
-        <h1>Test</h1>
-        <button
-          id="animated-like"
-          className="animated-like-button"
-          onClick={() => console.log("like")}
-        ></button>
+      <section id="about">
+        <iframe
+          title="fireworks-about"
+          src="https://v1.embednotion.com/embed/9022da6e4c85461286d70b7dfcbe19d3"
+        ></iframe>
       </section>
+
+      <section id="comments"></section>
     </>
   );
 }
