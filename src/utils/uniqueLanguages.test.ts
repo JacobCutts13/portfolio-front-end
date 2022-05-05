bd7fec974ea25212544c2e54d878e8d327b44a27
unknown <jacobdcutts@gmail.com>
2022-05-05 15:45:26 +0100

    updated tests to include likes

--- src/utils/uniqueLanguages.test.ts
+++ src/utils/uniqueLanguages.test.ts
@@ -10,6 +10,7 @@ const projectsExample = [
     image: "galaxy-sim-preview",
     create_date: "2021-05-01T23:00:00.000Z",
     difficulty: 8,
+    likes: 0
   },
   {
     id: 2,
@@ -20,6 +21,7 @@ const projectsExample = [
     image: "galaxy-sim-preview",
     create_date: "2021-05-01T23:00:00.000Z",
     difficulty: 8,
+    likes: 0
   },
 ];
 
