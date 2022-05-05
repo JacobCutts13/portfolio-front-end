b89569b72c6b87dbef875abe99af24a4c028e5a8
unknown <jacobdcutts@gmail.com>
2022-05-05 16:24:37 +0100

    removed test file

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import FireworksProject from "./components/projects/FireworksProject";
 import GameOfLifeProject from "./components/projects/GameOfLifeProject";
 import RayCastProject from "./components/projects/RayCastProject";
 import SimpleOrbitsProject from "./components/projects/SimpleOrbitsProject";
-import JqueryTest from "./components/projects/jqueryTest";
 import MainApp from "./MainApp";
 
 function App(): JSX.Element {
@@ -15,7 +14,6 @@ function App(): JSX.Element {
         <Route path="/Game-Of-Life" element={<GameOfLifeProject />} />
         <Route path="/Fireworks" element={<FireworksProject />} />
         <Route path="/Ray-Cast" element={<RayCastProject />} />
-        <Route path="/test" element={<JqueryTest name="test" />} />
       </Routes>
     </BrowserRouter>
   );
