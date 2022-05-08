022fd30bda926168e27eb7ac0bd06902adcd81a7
unknown <jacobdcutts@gmail.com>
2022-05-08 15:15:24 +0100

    updated tests to contain new Iproject type

--- src/utils/uniqueLanguages.test.ts
+++ src/utils/uniqueLanguages.test.ts
@@ -5,12 +5,11 @@ const projectsExample = [
     id: 1,
     title: "Galaxy Simulation",
     language: ["python", "c++"],
-    summary: "Simulating galaxy collisions using runge-kata methods",
-    description: "Simulating galaxy collisions using runge-kata methods",
-    image: "galaxy-sim-preview",
+    thumbnail_image: "galaxy-sim-preview",
     create_date: "2021-05-01T23:00:00.000Z",
     difficulty: 8,
     likes: 0,
+    iframe: "str",
   },
   {
     id: 2,
@@ -18,10 +17,11 @@ const projectsExample = [
     language: ["typescript", "python"],
     summary: "Simulating galaxy collisions using runge-kata methods",
     description: "Simulating galaxy collisions using runge-kata methods",
-    image: "galaxy-sim-preview",
+    thumbnail_image: "galaxy-sim-preview",
     create_date: "2021-05-01T23:00:00.000Z",
     difficulty: 8,
     likes: 0,
+    iframe: "str",
   },
 ];
 
